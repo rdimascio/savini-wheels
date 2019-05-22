8fe29db6bfefade3cbb5919230698753b6bfbbbb
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-05-22 03:20:22 -0700

    savini diamond posts per page hack

--- taxonomy-wheel_collections-savini-diamond.php
+++ taxonomy-wheel_collections-savini-diamond.php
@@ -26,19 +26,24 @@ $class = get_queried_object()->slug . '_grid';
 
 		<div class="<?= $class ?> grid flex justify-center align-center">
 
-			<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : 
+				
+				$count = 0;
+
+				?>
 
 				<?php
 				/* Start the Loop */
 				while ( have_posts() ) :
 					the_post();
 
-					/*
-					* Include the Post-Type-specific template for the content.
-					* If you want to override this in a child theme, then include a file
-					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					*/
-					get_template_part( 'template-parts/content', get_post_type() );
+					if ( $count <= 6 ) :
+
+						get_template_part( 'template-parts/content', get_post_type() );
+
+					endif;
+
+					$count++;
 
 				endwhile;
 
