81108221045140430bb4193e242e0ae578bfd0bc
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-08-15 20:34:14 -0700

    add vehicle builder

---
+++ page-vehicle-builder.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WP_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area wheel-builder">
+		<main id="main" class="site-main">
+
+            <div id="icf_page"></div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
+<script src="//iconfigurators.app/src/embed.cfm?ky=D4CF1097776A9AF580AE"></script>
