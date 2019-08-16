9d058e0d2785df133acbe99212f18c5e45c4ad21
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-08-15 20:03:23 -0700

    hero link fix

--- template-parts/front-hero.php
+++ template-parts/front-hero.php
@@ -1,7 +1,7 @@
 <div class="carousel-wrapper" id="hero-carousel">
 	<div class="carousel-inner">
 		<?php while ( has_sub_field( 'homepage_slider', 'option' ) ) : ?>
-			<div class="item" style="background-image: url(<?= the_sub_field( 'homepage_slider_background' ); ?>)" data-title="<?= the_sub_field( 'homepage_slider_title' ); ?>" data-caption="<?= the_sub_field( 'homepage_slider_caption' ); ?>" data-link="<?= ( the_sub_field( 'homepage_slider_link' ) ) ? get_permalink( the_sub_field( 'homepage_slider_link' ) ) : '/'; ?>" data-caption-position="<?= the_sub_field( 'homepage_slider_caption_position' ); ?>"></div>
+			<div class="item" style="background-image: url(<?= the_sub_field( 'homepage_slider_background' ); ?>)" data-title="<?= the_sub_field( 'homepage_slider_title' ); ?>" data-caption="<?= the_sub_field( 'homepage_slider_caption' ); ?>" data-link="<?= ( the_sub_field( 'homepage_slider_link' ) ) ? get_permalink( the_sub_field( 'homepage_slider_link' ) ) : null; ?>" data-caption-position="<?= the_sub_field( 'homepage_slider_caption_position' ); ?>"></div>
 		<?php endwhile; ?>
 	</div>
 </div>
